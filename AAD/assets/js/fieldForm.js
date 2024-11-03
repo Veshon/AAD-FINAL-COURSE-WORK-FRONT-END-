1774829aa7ac5ef040ff15875ae86980699e26c5
veshon <vashonmabima12@gmail.com>
2024-11-03 12:17:27 +0530

    Clear Fields Completed

--- AAD/assets/js/fieldForm.js
+++ AAD/assets/js/fieldForm.js
@@ -162,69 +162,18 @@ $(document).ready(function () {
             }
         });
     });
+
+    //////////////////////////////////Clear/////////////////////////////////////////
+    $("#clear").click(function() {
+        clearFields()
+    })
 });
 
+function clearFields() {
+    document.getElementById('fieldName').value = '';
+    document.getElementById('location').value = '';
+    document.getElementById('size').value = '';
+    document.getElementById('img1').value = '';
+    document.getElementById('img2').value = '';
+}
 
-/*
-$(document).ready(function () {
-    $("#save").click(function() {
-        event.preventDefault();
-
-        let idE = $("#fieldCode").val();
-        let nameE = $("#fieldName").val();
-        let locationE = $("#location").val();
-        let sizeE = $("#size").val();
-        // let cropE = $("#crop").val();
-        // let staffE = $("#staff").val();
-        let img1E = $("#img1").val();
-        let img2E = $("#img2").val();
-
-        console.log(idE);
-        console.log(nameE);
-        console.log(locationE);
-        console.log(sizeE);
-        // console.log(cropE);
-        // console.log(staffE);
-        console.log(img1E);
-        console.log(img2E);
-
-        const customerData = {
-            fieldCode:idE,
-            fieldName:nameE,
-            location:locationE,
-            size:sizeE,
-            // crop:cropE,
-            // staff:staffE,
-            img1:img1E,
-            img2:img2E
-        };
-
-        console.log(customerData)
-
-        const customerJSON = JSON.stringify(customerData)
-        console.log(customerJSON)
-
-        $.ajax({
-            url: "http://localhost:5050/fcw/api/v1/fields",
-            type: "POST",
-            data: customerJSON,
-            headers: { "Content-Type": "application/json" },
-/!*            success: (res) => {
-                console.log(JSON.stringify(res));
-                successBanner.classList.remove('hidden');
-                successBanner.style.display = 'block';
-                setTimeout(function() {
-                    successBanner.style.display = 'none';
-                }, 3000);
-            },
-            error: (res) => {
-                console.error(res);
-                notSuccessBanner.classList.remove('hidden');
-                notSuccessBannerSave.style.display = 'block';
-                setTimeout(function() {
-                    notSuccessBannerSave.style.display = 'none';
-                }, 3000);
-            }*!/
-        })
-    })
-})*/
